f63276cb77cef1b0fc5ba8130c0b2c5d201dc7ac
Mitri <hmirseid54@gmail.com>
2024-02-10 17:02:56 +0400

    pp

---
+++ math.js
@@ -0,0 +1,21 @@
+let number =  -81 ;
+
+// let modul = Math.abs(number) ;
+// ededi menfi den musbete gecirir
+
+// let kokAlti = Math.sqrt(modul) ;
+// ededi kokalti ya salir
+
+// let ustu = Math.pow(2, 5) ; 
+// Yeni ki, 2 ustu 5
+
+// let random = Math.random();
+// 1 den asagi eded secir
+
+// let limit = Math.floor(random * 5);
+// 0 den 5 e qeder ededi secir
+
+// let limit2 = Math.floor(random * 5) + 5 ;
+// 1 den 5 e qeder ededi secir
+
+console.log(limit2);
